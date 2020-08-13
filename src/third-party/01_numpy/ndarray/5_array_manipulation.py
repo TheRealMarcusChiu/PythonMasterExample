b12d8a793ffbcc95f233380c42db7aa9838213e0
Marcus Chiu <marcuschiu9@gmail.com>
2020-08-13 00:07:22 -0500

    added ndarray reshape

--- src/third-party/01_numpy/ndarray/5_array_manipulation.py
+++ src/third-party/01_numpy/ndarray/5_array_manipulation.py
@@ -1,10 +1,18 @@
 import numpy as np
 
-a = np.arange(8).reshape(2, 4)
-print('The original array:')
+a = np.arange(8).reshape(2, 2, 2)
+print('\nThe original array:')
 print(a)
 
-print('After applying the flat function:')
+a = a.reshape(2, 4)
+print('\nAfter reshape(2, 4):')
+print(a)
+
+a = a.reshape(8)
+print('\nAfter reshape(8):')
+print(a)
+
+print('\nAfter applying the flat function:')
 iterr = a.flat  # returns a 1-D iterator over the array
 print(iterr[5])
 
